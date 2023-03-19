3a7c354ec36db36c189fe7e084c5ec4479d64dbc
Joel Miceli <joelmiceliok@gmail.com>
2023-03-19 16:09:45 -0300

    subida de carpeta

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () =>{
+    return (
+    <nav>
+        <h3>Cacto Petshop</h3>
+        <div>
+            <button>Alimento para perros</button>
+            <button>Alimento para gatos</button>
+            <button>Camas e Indumentaria</button>
+            <button>Snacks</button>
+            <button>Juguetes</button>
+        </div>
+    </nav>
+    )
+}
+export default NavBar
