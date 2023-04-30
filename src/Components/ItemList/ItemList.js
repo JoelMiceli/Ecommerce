0bb1be438ed579a7e5073f978a039ba1dc4501a6
Joel Miceli <joelmiceliok@gmail.com>
2023-04-30 16:59:16 -0300

    cambios y agregados

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -4,7 +4,7 @@ import Item from "../Item/Item";
 const ItemList = ({ products }) => {
   return (
     <div className="prodDestacados">
-      <h1><span class="badge text-bg-success">⭐ Productos Destacados⭐</span></h1>
+      <h1><span class="badge text-bg-success">⭐Productos Destacados⭐</span></h1>
     <div className="ListGroup">
       {products.slice(0,6).map((prod) => (
       <Item key={prod.id} {...prod} />
